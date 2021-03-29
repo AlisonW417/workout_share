d51049382a63facae5a78d96688b41e0d755187e
Alison Williams <apw5011@gmail.com>
2021-03-28 21:46:13 -0400

    added sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :workouts 
     has_many :comments
     has_many :commented_workouts, through: :comments, source: :workout
+
+    has_secure_password
 end
