021c339489fbe6eb335275a56a577ebba51b546d
Alison Williams <apw5011@gmail.com>
2021-03-29 17:37:56 -0400

    added sessions#create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,13 @@ class SessionsController < ApplicationController
     end 
 
     def create 
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(password: params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to welcome_path #change to user show page
+        else 
+            redirect_to login_path
+        end 
     end 
 
     def destroy 
