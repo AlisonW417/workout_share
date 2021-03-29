70683ccfa433a964b665c830a78ec8851e4cd506
Alison Williams <apw5011@gmail.com>
2021-03-29 17:33:37 -0400

    added sign up and log in forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+    def welcome 
+    end 
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def destroy 
+        session.delete(:user_id)
+        redirect_to '/'
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :comments
   resources :workouts
   resources :users
-  get '/', to: 'sessions#welcome'
+
+  get '/', to: 'sessions#welcome', as: 'welcome'
+  get '/login', to: 'sessions#new' 
+  post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
