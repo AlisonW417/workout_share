c7b28b67ede538d4115e8404f84e4457146f4621
Alison Williams <apw5011@gmail.com>
2021-03-30 16:45:17 -0400

    added edit workout function

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -18,6 +18,19 @@ class WorkoutsController < ApplicationController
         @workout = Workout.find_by(id: params[:id])
     end 
 
+    def edit 
+        @workout = Workout.find_by(id: params[:id])
+    end 
+
+    def update 
+        @workout = Workout.find_by(id: params[:id])
+        if @workout.update(workout_params)
+            redirect_to workout_path(@workout)
+        else 
+            render :edit 
+        end 
+    end 
+
     def workout_params 
         params.require(:workout).permit(:name, :date_completed, :duration, :cardio, :strength, :average_heart_rate, :total_calories, :description)
     end 
