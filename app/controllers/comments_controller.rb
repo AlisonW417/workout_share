54a63e684bb796fb0519eb2c8668f46c462c4b58
Alison Williams <apw5011@gmail.com>
2021-03-30 17:14:10 -0400

    added current_user method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+    def new 
+    end 
+
+    def create 
+        raise params.inspect
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save 
+            redirect_to welcome_path 
+        end
+    end 
+
+    def comment_params 
+        params.require(:comment).permit(:description)
+    end 
 end
