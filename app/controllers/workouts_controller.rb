c4d6def8dde33eb47fe79609a850d45964f456f8
Alison Williams <apw5011@gmail.com>
2021-03-30 16:10:34 -0400

    added workout create and show functions

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,24 @@
 class WorkoutsController < ApplicationController
+
+    def new 
+        @workout = Workout.new
+    end 
+
+    def create 
+        @workout = Workout.new(workout_params)
+        @workout.user_id = session[:user_id]
+        if @workout.save 
+            redirect_to workout_path(@workout)
+        else 
+            render :new
+        end 
+    end 
+
+    def show 
+        @workout = Workout.find_by(id: params[:id])
+    end 
+
+    def workout_params 
+        params.require(:workout).permit(:name, :date_completed, :duration, :cardio, :strength, :average_heart_rate, :total_calories, :description)
+    end 
 end
