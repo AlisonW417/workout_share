0c106e1a47c325afe7aed0479ed217332f894f4e
Alison Williams <apw5011@gmail.com>
2021-03-31 22:07:34 -0400

    added errors partial and workout validations

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,9 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+
+  validates :name, presence: true 
+  validates :date_completed, presence: true 
+  validates :duration, presence: true 
+  validates :description, presence: true 
 end
