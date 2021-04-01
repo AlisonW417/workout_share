739af09d72b1e41f48c7fca2d12dfa85af98d53e
Alison Williams <apw5011@gmail.com>
2021-03-31 22:34:44 -0400

    added errors to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
         else 
-            redirect_to signup_path
+            render :new 
         end 
     end 
 
