a8bea07868c95a4c66f5352e4df7289198e7ace6
Alison Williams <apw5011@gmail.com>
2021-04-05 09:26:03 -0400

    comments display header helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,9 @@
 module CommentsHelper
-end
+    def display_header(comment)
+        if params[:workout_id]
+            content_tag(:h1, "Give #{@workout.user.username} kudos on #{@workout.name}")
+        else 
+            content_tag(:h1, "Create a Comment")
+        end 
+    end 
+end 
